0a60db7e03ab9515825778272cbc83756148919e
dakotasblack <kotab@me.com>
2016-03-29 15:54:19 -0500

    Update Program.cs
    Added Aurelion Sol

--- AutoLvLUp/Program.cs
+++ AutoLvLUp/Program.cs
@@ -81,6 +81,9 @@ namespace AutoSpellUp
                 case "Ashe":
                     AbilitySequence = new[] { 2, 1, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
                     break;
+                case "AurelionSol":
+                    AbilitySequence = new[] { 1, 2, 1, 3, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
+                    break;
                 case "Azir":
                     AbilitySequence = new[] { 2, 1, 3, 2, 2, 4, 1, 2, 2, 1, 4, 1, 3, 1, 3, 4, 3, 3 };
                     break;
